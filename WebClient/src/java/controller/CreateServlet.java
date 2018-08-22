8c16ee29c0a1edcbe11161214d1eb35a70900e91
DESKTOP-LIL0IE5\salar <duylkd00320@fpt.edu.vn>
2018-08-22 20:33:34 +0700

    no message

---
+++ WebClient/src/java/controller/CreateServlet.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import service.NewJerseyClient;
+import entity.Users;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.GenericType;
+
+/**
+ *
+ * @author SYSTEM
+ */
+public class CreateServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("usermanagement.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("txtusername");
+        String password = request.getParameter("txtpassword");
+        int role = Integer.parseInt(request.getParameter("role"));
+       
+        NewJerseyClient client = new NewJerseyClient();
+        GenericType<List<Users>> gType = new GenericType<List<Users>>(){};
+        Users u = new Users();
+        u.setUsername(username);
+        u.setPassword(password);
+        u.setRole(role);
+        client.create_XML(u);
+        List<Users> users = (List<Users>) client.findAll_XML(gType);
+        request.setAttribute("ListUser", users);
+        request.getRequestDispatcher("usermanagement.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+
+}
